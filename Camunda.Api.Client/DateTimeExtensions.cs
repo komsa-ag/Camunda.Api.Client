a3f392458651fa5ec76f7a4c5a5f8bd6e46231be
Jan Friedrich <jan.friedrich@komsa.de>
2024-11-08 12:00:37 +0100

    Refit 8.0 - fixes #32

--- Camunda.Api.Client/DateTimeExtensions.cs
+++ Camunda.Api.Client/DateTimeExtensions.cs
@@ -10,16 +10,16 @@ using System.Runtime.CompilerServices;
 "435db7ae")]
 namespace Camunda.Api.Client
 {
-    internal static class DateTimeExtensions
-    {
-        const string _dateTimeFormat = "yyyy-MM-ddTHH':'mm':'ss.fff";
+  internal static class DateTimeExtensions
+  {
+    const string _dateTimeFormat = "yyyy-MM-ddTHH':'mm':'ss.fff";
 
-        public static string ToJavaISO8601(this DateTime dateTime)
-        {
-            if (dateTime.Kind == DateTimeKind.Unspecified) // treat unspecified time as local time
-                dateTime = new DateTime(dateTime.Ticks, DateTimeKind.Local);
-            return dateTime.ToString(_dateTimeFormat, CultureInfo.InvariantCulture)
-                + dateTime.ToString("%K").Replace(":", "").Replace("Z", "+0000");
-        }
+    public static string ToJavaISO8601(this DateTime dateTime)
+    {
+      if (dateTime.Kind == DateTimeKind.Unspecified) // treat unspecified time as local time
+        dateTime = new DateTime(dateTime.Ticks, DateTimeKind.Local);
+      return dateTime.ToString(_dateTimeFormat, CultureInfo.InvariantCulture)
+        + dateTime.ToString("%K").Replace(":", "").Replace("Z", "+0000");
     }
+  }
 }
